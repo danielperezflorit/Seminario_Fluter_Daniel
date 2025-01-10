7e2766f80ea9ca38674665180bcf396e12bf6ed8
danielperezflorit <daniel.perez.florit@estudiantat.upc,edu>
2025-01-10 20:47:55 +0100

    flutter dockerizado

--- API_REST/src/mongo.js
+++ API_REST/src/mongo.js
@@ -3,7 +3,7 @@
 const mongoose = require ('mongoose')
 //import mongoose from 'mongoose'
 
-const connectionString = 'mongodb://localhost:27017/Prueba_1'
+const connectionString = 'mongodb://mongodb:27017/Prueba_1'
 
 mongoose.connect(connectionString)
     .then(()=>{
